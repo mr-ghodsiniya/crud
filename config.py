5f55287238ce4d128662860790921cabd61a0c5a
Mr-Ghodsiniya <amirghodsiniya4444@gmail.com>
2023-08-03 21:13:38 +0330

    done

---
+++ config.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from routers import users , tasks
+import time
+import datetime
+
+
+app = FastAPI()
+app.include_router(users.router, tags=['users'])
+app.include_router(tasks.router, tags=['tasks'])
+
+
+@app.middleware('http')
+async def add_process_time_header(request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers['X-Process-Time'] = str(process_time)
+    return response
+
+
+@app.on_event('startup')
+def startup_event():
+    with open('server_time_log.log', 'a') as log:
+        log.write(f'Application started at: {datetime.datetime.now()} \n')
+       
+       
+@app.on_event('shutdown')
+def shutdown_event():
+    with open('server_time_log.log', 'a') as log:
+        log.write(f'Application shut down at: {datetime.datetime.now()} \n')
