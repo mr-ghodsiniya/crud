3c436453d8e0dbc8469c829bf062165f2a601240
Mr-Ghodsiniya <amirghodsiniya4444@gmail.com>
2023-08-05 14:47:01 +0330

    edit column

--- models/tasks_m.py
+++ models/tasks_m.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, String, Integer, Boolean
 class Task(Base):
     __tablename__ = "tasks"
     
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, unique=True)
     title = Column(String)
     description = Column(String)
     completed = Column(Boolean, default=False)
